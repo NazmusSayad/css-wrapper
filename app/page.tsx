b15c08c8f79a6e9935aecef566329b53623bc373
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-07-21 21:00:47 +0600

    refactor: streamline HomePage by removing PaddingWrapperPlayground
    - Eliminated PaddingWrapperPlayground and associated elements from HomePage for a cleaner layout.
    - Retained MaxWidthWrapperPlayground to maintain functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,12 @@
 
 export const dynamic = 'force-static'
 
-import { MaxWidthWrapperPlayground, PaddingWrapperPlayground } from '@/features/playground'
+import { MaxWidthWrapperPlayground } from '@/features/playground'
 
 export default function HomePage() {
   return (
     <>
-      <PaddingWrapperPlayground />
-      <hr />
       <MaxWidthWrapperPlayground />
-      <hr />
     </>
   )
 }
